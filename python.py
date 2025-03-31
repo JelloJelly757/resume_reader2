af2200c4134c3a6bcbea67db49a2314e4a9f66d4
Maya <mayas2744@tuckahoeschools.org>
2025-03-31 12:33:02 +0000

    int commit

--- python.py
+++ python.py
@@ -15,10 +15,14 @@ for pdfName in resumeNameList:
     num_pages = len(reader.pages)
 
     # define key terms
-    string1.lower() == "leadership".lower() #college education
-    string2.lower() == "university".lower() #skills
-    string3.lower() == "python".lower() #experiences
-    string4.lower() == "GPA".lower() #certifications
+    string1 = "leadership" #college education
+    x = string1.lower()
+    string2 = "university" #skills
+    x = string2.lower()
+    string3 = "python" #experiences
+    x = string3.lower()
+    string4 = "GPA"   #certifications
+    x = string4.lower()
 
     # extract text and do the search
     for page in reader.pages:
@@ -33,7 +37,7 @@ for pdfName in resumeNameList:
         res_search = re.findall(string4, text)
         print(res_search)
 
-    my_dict = {"leadership":   1, "university": 2, "python": 3, "GPA": 4}
+    my_dict = {"leadership": 1, "university": 2, "python": 3, "GPA": 4}
 
     def get_points_for_pdf(words):
         total_points = 0
@@ -47,6 +51,7 @@ for pdfName in resumeNameList:
     for page in reader.pages:
         text = page.extract_text()
         words = text.split()  # Split the text into individual words
+        #print(f"Intial total point value {total_points}")
         total_points = get_points_for_pdf(words)
         print(f"Total points for this page: {total_points}")
         total_pdf_points += total_points
@@ -54,3 +59,8 @@ for pdfName in resumeNameList:
     pointArray.append(total_pdf_points)
     pointArray.append("|")
 print(pointArray)
+
+#we need to make it lower caps accessible + the ending needs to say Resume one has: __ points, etc 
+#can the words in the pdf be case sensitive or does it have to be exactly like the pdf?
+#comment more code need to + we s to be multi points? #we need to get thin
+#ending needs to say Resume __ is the most qualified with __ points 
